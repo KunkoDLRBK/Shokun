d5da358fa438eddc711694d6d8b1b63a7763d259
Kunko <88788880+KunkoDLRBK@users.noreply.github.com>
2023-09-01 13:49:38 +0200

    Layout.js

--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -6,7 +6,7 @@ const Layout = () =>{
  return <div>
     <nav class=" navbar bg-dark">
         <Link to ="/" class=""><img src={logo} alt="Logo shokun" width="15%"></img></Link>
-        <Link to="/usuario"></Link>
+        <Link to="/usuario"><img src={logo} alt="Logo shokun" width="15%"></img></Link>
         <Link to="/inicio"></Link>
         <Link to="/login" class="float-right"><img src={us} alt="Usuario" class="justify-content-end" width="15%"></img></Link>
       </nav>
